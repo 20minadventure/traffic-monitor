73622658e88353561a9e82a267958e3f4e9fe6cc
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-03-08 19:55:09 +0100

    Add simple dry_run test for TrafficDetector

--- tests/test_traffic_detection.py
+++ tests/test_traffic_detection.py
@@ -1,2 +1,12 @@
-def test_import():
-    from traffic_detection import TrafficDetector
+from traffic_detection import TrafficDetector
+
+
+def test_dry_run_prediction(tmp_path):
+    clip_path = tmp_path / 'clip.mp4'
+
+    td = TrafficDetector(clip_path)
+    td.detect_vehicles()
+
+    assert hasattr(td, 'confs')
+    assert hasattr(td, 'boxes')
+    assert hasattr(td, 'class_names')
