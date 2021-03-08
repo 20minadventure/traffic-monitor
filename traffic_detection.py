78909484eb734b37dcc2c020e0283a636c02ced1
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-03-08 20:34:31 +0100

    Add loading a yolov4 model in TrafficDetector

--- traffic_detection.py
+++ traffic_detection.py
@@ -1,3 +1,19 @@
+import cv2
+from pathlib import Path
+
+
 class TrafficDetector:
     def __init__(self, path):
         self.path = path
+        self._model = None
+
+    @property
+    def model(self):
+        if self._model is None:
+            nn_weights_path = Path('yolov4', 'yolov4.weights')
+            nn_cfg_path = Path('yolov4', 'yolov4.cfg')
+            net = cv2.dnn.readNet(str(nn_weights_path), str(nn_cfg_path))
+            model = cv2.dnn_DetectionModel(net)
+            model.setInputParams(size=(512, 512), scale=1/255)
+            self._model = model
+        return self._model
