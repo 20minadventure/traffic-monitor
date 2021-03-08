ee6534e35ee41eac2dadbc101a91e752188fe072
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-03-08 20:33:08 +0100

    Add test for a model loading

--- tests/test_traffic_detection.py
+++ tests/test_traffic_detection.py
@@ -1,6 +1,15 @@
+import cv2
 import pytest
+
+from traffic_detection import TrafficDetector
 
 
 @pytest.fixture
 def dummy_clip_path(tmp_path):
     return tmp_path / 'dummy_clip.mp4'
+
+
+def test_model_loading(dummy_clip_path):
+    td = TrafficDetector(dummy_clip_path)
+
+    assert isinstance(td.model, cv2.dnn_DetectionModel)
