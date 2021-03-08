ad258848db829df63f5f076595da04f6065c36c1
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-03-08 20:15:00 +0100

    Add a function for a clip reading

--- traffic_detection.py
+++ traffic_detection.py
@@ -1,6 +1,20 @@
+import cv2
+
+
 class TrafficDetector:
     def __init__(self, path):
         self.path = path
         self.confs = None
         self.boxes = None
         self.class_names = None
+
+    def iter_clip_frames(self):
+        clip = cv2.VideoCapture(str(self.path))
+        clip = cv2.VideoCapture(str(path))
+        while True:
+            success, frame = clip.read()
+            if success:
+                yield frame
+            else:
+                break
+        clip.release()
