3d4f44b996a539df2e32b97e37c495c01b948a5f
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-03-08 20:32:34 +0100

    Create a fixture for a path to the example clip

--- tests/test_traffic_detection.py
+++ tests/test_traffic_detection.py
@@ -1,10 +1,15 @@
+import pytest
+
 from traffic_detection import TrafficDetector
 
 
-def test_dry_run_prediction(tmp_path):
-    clip_path = tmp_path / 'clip.mp4'
+@pytest.fixture
+def example_clip_path(tmp_path):
+    return tmp_path / 'clip.mp4'
 
-    td = TrafficDetector(clip_path)
+
+def test_dry_run_prediction(example_clip_path):
+    td = TrafficDetector(example_clip_path)
     td.detect_vehicles()
 
     assert hasattr(td, 'confs')
