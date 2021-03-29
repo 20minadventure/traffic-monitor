4cd3051f464d5a410a5d814fa4e946515c6cc6f2
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-03-29 20:07:49 +0200

    Generate patches from an image

--- traffic_detection.py
+++ traffic_detection.py
@@ -1,7 +1,50 @@
 import cv2
+import numpy as np
 from pathlib import Path
 
 
+def patch_generator(image, patch_size, patches=None, min_padding=0,
+        start_point=(0, 0), end_point=None):
+    if end_point is None:
+        end_point = image.shape[1], image.shape[0]
+    width, height = end_point[0] - start_point[0], end_point[1] - start_point[1]
+    if min(width, height) < patch_size or min_padding >= patch_size:
+        return
+
+    if patches is None:
+        p = np.ceil(
+            (width - min_padding) / (patch_size - min_padding)
+        ).astype(int)
+        q = np.ceil(
+            (height - min_padding) / (patch_size - min_padding)
+        ).astype(int)
+        min_padding_x = min_padding_y = min_padding
+    else:
+        q, p = patches
+        if q * patch_size < height or p * patch_size < width:
+            return
+        min_padding_x, min_padding_y = (0, 0)
+        if p != 1:
+            min_padding_x = int((p * patch_size - width) / (p - 1))
+        if q != 1:
+            min_padding_y = int((q * patch_size - height) / (q - 1))
+
+    x0, y0 = start_point
+    for i in range(q):
+        for j in range(p):
+            xn = x0 + j * (patch_size - min_padding_x)
+            yn = y0 + i * (patch_size - min_padding_y)
+
+            xn -= max((xn + patch_size - x0) - width, 0)
+            yn -= max((yn + patch_size - y0) - height, 0)
+
+            patch = image[
+                yn : yn + patch_size,
+                xn : xn + patch_size
+            ]
+            yield patch, (xn, yn)
+
+            
 class TrafficDetector:
     def __init__(self, path):
         self.path = path
