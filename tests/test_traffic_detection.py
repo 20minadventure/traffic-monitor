36c517a150f663fbef104f012c6806cf3ddf2c5f
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-04-06 17:34:36 +0200

    Add tests for a clip's frames iterator

--- tests/test_traffic_detection.py
+++ tests/test_traffic_detection.py
@@ -19,3 +19,25 @@ def test_model_loading(dummy_clip_path):
     td = TrafficDetector(dummy_clip_path)
 
     assert isinstance(td.model, cv2.dnn_DetectionModel)
+
+
+def test_rasing_error_when_clip_doesnt_exist(dummy_clip_path):
+    td = TrafficDetector(dummy_clip_path)
+
+    with pytest.raises(FileNotFoundError):
+        next(td.iter_clip_frames())
+
+
+@pytest.mark.parametrize(
+   "count, step",
+   [(None, 100), (10, 5), (0, 1)]
+)
+def test_iter_clip_frames(real_clip_path, count, step):
+    td = TrafficDetector(real_clip_path, count=count, step=step)
+    if count is None:
+        count = 331
+
+    frames = [frame for frame, _ in td.iter_clip_frames()]
+
+    assert frames == list(range(0, count, step))
+
