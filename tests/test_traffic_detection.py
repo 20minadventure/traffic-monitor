c76c1949ca508286d5d3f3b534fe527ec093836a
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2021-04-17 16:28:04 +0200

    Test detection when there're no vehicles on the clip

--- tests/test_traffic_detection.py
+++ tests/test_traffic_detection.py
@@ -91,3 +91,14 @@ def test_detection(real_clip_path):
     assert len(td.scores) == 1
     assert len(td.boxes) == 1
     assert len(td.classes_ids[0])
+
+
+def test_detection_on_black_clip(black_clip_path):
+    td = TrafficDetector(black_clip_path, count=1)
+
+    td.detect_vehicles()
+
+    assert len(td.classes_ids) == 1
+    assert len(td.scores) == 1
+    assert len(td.boxes) == 1
+    assert len(td.classes_ids[0]) == 0
